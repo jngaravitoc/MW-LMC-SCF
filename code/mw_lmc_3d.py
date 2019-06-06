f5db2713f860fe4c3adaac9052322934b4624e41
jngaravitoc <jngaravitoc@email.arizona.edu>
2019-06-06 12:25:52 -1000

    3d plot update

--- code/mw_lmc_3d.py
+++ code/mw_lmc_3d.py
@@ -48,9 +48,63 @@ def counter_orbiting_sats(names):
 
     print(names[index_name.astype(int)])
     return index_name.astype(int)
-   
 
 
+
+def lmc_mw_ratio_potetial():
+    coeff_c = np.loadtxt('../../SCF_tools/PCA/MWLMC5_coeff_20_20_100M_b1.txt')
+    S = coeff_c[:,0]
+    T = coeff_c[:,1]
+
+    S_matrix = np.zeros((21, 21, 21))
+    T_matrix = np.zeros((21, 21, 21))
+
+
+    counter = 0
+    for n in range(21):
+        for l in range(21):
+            for m in range(0, l+1):
+                S_matrix[n][l][m] = S[counter]
+                T_matrix[n][l][m] = T[counter]
+                counter +=1
+                
+    ## Reading coefficients 
+    coeff_lmc = np.loadtxt('./LMC/coeff_rand_40lmc5_b1_1E6_1.txt')
+    S_lmc = coeff_lmc[:,0]
+    T_lmc = coeff_lmc[:,1]
+
+    S_lmc_1e6 = np.zeros((41, 21, 21))
+    T_lmc_1e6 = np.zeros((41, 21, 21))
+
+
+    counter = 0
+    for n in range(41):
+        for l in range(21):
+            for m in range(0, l+1):
+                S_lmc_1e6[n][l][m] = S_lmc[counter]
+                T_lmc_1e6[n][l][m] = T_lmc[counter]
+                counter +=1
+
+
+    xlmc_com = 1
+    ylmc_com = -41
+    zlmc_com = -28
+    
+    x_grid = np.arange(-300, 300, 5.5)
+    y_grid = np.arange(-300, 300, 5.5)
+    z_grid = np.arange(-300, 300, 5.5)
+    X_grid, Y_grid, Z_grid = np.meshgrid(x_grid, y_grid, z_grid)
+    
+    pot_ratio_all = np.zeros((110, 110, 110))
+    for i in range(110):
+        for j in range(110):
+            for k in range(110):
+                pot_ratio_all[i][j][k] = biff.potential(np.array([[X_grid[0][i][0]-xlmc_com], [Y_grid[j][0][0]-ylmc_com], [Z_grid[0][0][k]-zlmc_com]]).T,
+                                          S_lmc_1e6, T_lmc_1e6, M=11.41*1E6,r_s=10, G=1) / \
+                                      biff.potential(np.array([[X_grid[0][i][0]], [Y_grid[j][0][0]], [Z_grid[0][0][k]]]).T, S_matrix,
+                                          T_matrix, M=1, r_s=40.85, G=1)
+                                      
+    return pot_ratio_all
 if __name__ == "__main__":
 
     all_sats = read_satellites_6d_coords('../data/MW_satellites.dat')
@@ -67,8 +121,13 @@ if __name__ == "__main__":
     co_orb = co_orbiting_sats(names)
     counter_orb = counter_orbiting_sats(names)
     
+    pot_lmc = lmc_mw_ratio_potetial()
+    savetxt('pot_lmc_mw3d.txt', pot_lmc)
     mlab.figure(bgcolor=(0.0, 0.0, 0.0))
 
+    tt = mlab.contour3d(pot_lmc, contours=20,
+               opacity=.3, extent=[-300, 300, -300, 300, -300, 300],
+                   transparent=True, colormap='viridis')
 
     mlab.points3d(x_sats, y_sats, z_sats, color=(1,0,0),\
                   scale_factor=10, opacity=1)
@@ -91,6 +150,8 @@ if __name__ == "__main__":
 
     mlab.points3d(0, 0, 0, 20,  mode='2dcircle', color=(1,1,1),\
                  scale_factor=1.0, opacity=1, resolution=100)
+                 
+              
 
     mlab.savefig('mw_satellites_3d.png', size=(200, 200))
 
@@ -124,117 +185,4 @@ r_s_sims = 40.85
 xlmc_com = 1
 ylmc_com = -41
 zlmc_com = -28
-
-
-
-coeff_c = np.loadtxt('../../SCF_tools/PCA/MWLMC5_coeff_20_20_100M_b1.txt')
-S = coeff_c[:,0]
-T = coeff_c[:,1]
-
-S_matrix = np.zeros((21, 21, 21))
-T_matrix = np.zeros((21, 21, 21))
-
-
-counter = 0
-for n in range(21):
-    for l in range(21):
-        for m in range(0, l+1):
-            S_matrix[n][l][m] = S[counter]
-            T_matrix[n][l][m] = T[counter]
-            counter +=1
-
-
-coeff_lmc = np.loadtxt('./LMC/coeff_rand_40lmc5_b1_1E6_1.txt')
-S_lmc = coeff_lmc[:,0]
-T_lmc = coeff_lmc[:,1]
-
-S_lmc_1e6 = np.zeros((41, 21, 21))
-T_lmc_1e6 = np.zeros((41, 21, 21))
-
-
-counter = 0
-for n in range(41):
-    for l in range(21):
-        for m in range(0, l+1):
-            S_lmc_1e6[n][l][m] = S_lmc[counter]
-            T_lmc_1e6[n][l][m] = T_lmc[counter]
-            counter +=1
-
-
-x_grid = np.arange(-300, 300, 5)
-y_grid = np.arange(-300, 300, 5)
-z_grid = np.arange(-300, 300, 5)
-X_grid, Y_grid, Z_grid = np.meshgrid(x_grid, y_grid, z_grid)
-
 """
-
-#a_ratio_all = np.zeros((110, 110))
-#for i in range(110):
-#    print(y_grid[i])
-#    for j in range(110):
-#        print(z_grid[j], zlmc_com)
-#        xyz_lmc = (np.array([[0-xlmc_com], [y_grid[i]-ylmc_com],[z_grid[j]-zlmc_com]]))
-#        print(xyz_lmc.astype(float))
-#        print(biff.gradient(xyz_lmc.astype(float), S_lmc_1e6, T_lmc_1e6, M=11.41*1E6, r_s=10, G=1))
-
-        #a_all_lmc = biff.gradient(np.array([[0-xlmc_com], [y_grid[0][i]-ylmc_com], [z_grid[j,0]-zlmc_com]]).T,
-        #                      S_lmc_1e6, T_lmc_1e6, M=11.41*1E6,r_s=10, G=1) 
-        #a_all_mw = biff.gradient(np.array([[0], [y_grid[0][i]], [z_grid[j,0]]]).T, S_matrix,
-        #                      T_matrix, M=1, r_s=40.85, G=1)
-                              
-        #a_all[i][j] = np.sqrt(a_all_lmc[0][0]**2 + a_all_lmc[0][1]**2 +\
-        #                      a_all_lmc[0][2]**2) +  np.sqrt(a_all_mw[0][0]**2 + \
-        #                      a_all_mw[0][1]**2 + a_all_mw[0][2]**2)
-
-
-
-#np.savetxt('a_mwlmc.txt', a_ratio_all)
-
-
-
-#rho_mwlmc = np.loadtxt('rhomwlmc.txt')
-#rho_mwlmc_base = np.loadtxt('rhomwlmc_base.txt')
-
-#rho_matrix = np.reshape(rho_mwlmc, (120, 120, 120))
-#rho_base = np.reshape(rho_mwlmc_base, (120, 120, 120))
-
-### lmc orbit
-
-"""
-LMC5_b1 = '../../MW_anisotropy/data/orbits/LMC5_100Mb1_orbit.txt'
-LMC5_b1_orbit = reading_Nbody(LMC5_b1)
-
-x_sat = LMC5_b1_orbit[1]
-y_sat = LMC5_b1_orbit[2]
-z_sat = LMC5_b1_orbit[3]
-x_gal = LMC5_b1_orbit[4]
-y_gal = LMC5_b1_orbit[5]
-z_gal = LMC5_b1_orbit[6]
-"""
-
-
-#mlab.figure(bgcolor=(0.0, 0.0, 0.0))
-
-#mlab.plot3d(y_sat[:111]-y_gal[:111]+2.5, x_sat[:111]-x_gal[:111]+2.5, z_sat[:111]-z_gal[:111]+2.5, 
-#            np.ones(len(x_sat[:111])), color=(1,0,0), line_width=200,
-#            tube_radius=2, opacity=1)
-
-#mlab.points3d(y_sat[111]-y_gal[111] +2.5, x_sat[111]-x_gal[111]+2.5, z_sat[111]-z_gal[111]+2.5,
-#             100, color=(1,0,0), scale_factor=2, opacity=1)
-#
-#mlab.points3d(2.5, 2.5, 2.5,
-#             100,  mode='2dcircle', color=(1,0,0), scale_factor=1.0, opacity=1,
-#             resolution=40, line_width=180)
-
-#tt = mlab.contour3d((rho_matrix/rho_base)-1,contours=12,
-#                    opacity=.3, vmin=-0.45, vmax=0.45, extent=[-300, 300, -300, 300, -300, 300],
-#                    transparent=True)#, colormap='viridis')
-
-
-
-#for i in range(0, 1):
-#    mlab.view(azimuth=i, elevation=90, distance=1200)
-#    mlab.savefig('all_a.png',
-#            size=(4000,4000))
-
-#mlab.close()
